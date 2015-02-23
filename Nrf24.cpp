59010c0aa4222785a917b01d474f6b00ce560853
nasir-ahmad <nasirmail10@gmail.com>
2015-02-23 13:00:25 +0600

    First pre alpha release

---
+++ Nrf24.cpp
@@ -0,0 +1,303 @@
+/******************************************************************************
+ * The MIT License
+ *
+ * Copyright (c) 2014 Nasir Ahmad.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *****************************************************************************/
+
+/**
+ *  @brief NRF24L01+ RF module library for LeafLabs Maple
+ */
+
+
+#include "Nrf24.h"
+
+Nrf24::Nrf24(HardwareSPI *spi_dev, uint8 csn_pin, uint8 ce_pin){		
+	_port.csn = csn_pin;
+	_port.ce = ce_pin;
+	_port.spi = spi_dev;
+	
+	pinMode(_port.csn, OUTPUT);
+	pinMode(_port.ce, OUTPUT);
+	
+	_dynamicPayload = false;
+	_useInterrupt = false;
+	_onPayloadReceiveCallback = NULL;
+}
+
+
+void Nrf24::begin(Nrf24Mode mode){		
+	digitalWrite(_port.csn, HIGH);
+	digitalWrite(_port.ce, LOW);			
+	_port.spi->begin(SPI_9MHZ, MSBFIRST, 0);		
+	
+	//Power on reset needs at least 10.3mS	
+	delay(11);	
+	
+	//By default enable pipe0
+	setPipe(0, 32);
+	setMode(mode);
+}
+
+
+uint8 Nrf24::getIRQPin(){
+	return _port.irq;
+}
+
+void Nrf24::onInterrupt(){
+	if (!_useInterrupt) return;
+	
+	//Avoid triggering in the middle of high to low transition
+	delayMicroseconds(1);
+	if(digitalRead(_port.irq) == 0){
+		_fetchStatus();
+	}
+}
+
+void Nrf24::useIRQPin(uint8 pin, bool enable){
+	_useInterrupt = enable;
+	_port.irq = pin;	
+	pinMode(_port.irq, INPUT);
+}
+
+
+uint8 Nrf24::txDataSent(){	
+	if(!_useInterrupt)		
+		_fetchStatus();
+	
+	//Make sure the device is connected properly
+	if (_status != 0xFF){
+		if(_status & NRF24_STATUS_MAX_RT){
+			writeRegister(NRF24_REG_STATUS, NRF24_STATUS_MAX_RT);
+			return 2;
+		} else if (_status & NRF24_STATUS_TX_DS){
+			writeRegister(NRF24_REG_STATUS, NRF24_STATUS_TX_DS);
+			return 1;
+		}	
+	}
+	return 0;
+}
+
+void Nrf24::attachOnPayloadReceiveCallback(void (*callback)(void)){
+	_onPayloadReceiveCallback = callback;
+}
+
+void Nrf24::_fetchStatus(){
+	_status = readRegister(NRF24_REG_STATUS);	
+	if ((_status & NRF24_STATUS_RX_DR) && _onPayloadReceiveCallback){
+		_onPayloadReceiveCallback();
+	}
+}
+
+uint8 Nrf24::available(){
+	if(!_useInterrupt)		
+		_fetchStatus();		
+	
+	//Make sure the device is connected properly
+	if ((_status != 0xFF) && (_status & NRF24_STATUS_RX_DR)){
+		uint8 pipe = _status & NRF24_STATUS_RX_P_NO; //range = 0 - 5
+		uint8 length;								 //range = 1 - 32		
+		if (_dynamicPayload){			
+			execCmd(NRF24_CMD_R_RX_PL_WID, NRF24_READ, &length, 1);			
+		} else {
+			length = readRegister(NRF24_REG_RX_PW_P0 + pipe);
+		}		
+		return length;
+	}
+	return 0;
+}
+
+void Nrf24::read(byte *buffer, uint8 length){
+	execCmd(NRF24_CMD_R_RX_PAYLOAD, NRF24_READ, buffer, length);
+	writeRegister(NRF24_REG_STATUS, NRF24_STATUS_RX_DR);		
+}
+
+void Nrf24::send(byte* buffer, uint8 length){
+	
+	flushTx();
+	writeRegister(NRF24_REG_STATUS, NRF24_STATUS_MAX_RT);
+	
+	digitalWrite(_port.ce, LOW);
+	digitalWrite(_port.csn, LOW);
+	_port.spi->write(NRF24_CMD_W_TX_PAYLOAD);
+	
+	while (length--){
+	 _port.spi->write(*buffer++);
+	}	 
+	digitalWrite(_port.csn, HIGH);
+
+	//Toggle CE pin low to high for >10uS to initiate sending from tx buffer
+	digitalWrite(_port.ce, HIGH);
+	delayMicroseconds(15);
+	digitalWrite(_port.ce, LOW);	 
+}
+
+
+void Nrf24::setPayloadSize(uint8 pipe, uint8 length){
+	//Pipe starts from 0 to 5
+	if (pipe < 6)
+		writeRegister(NRF24_REG_RX_PW_P0 + pipe, length);
+}
+
+void Nrf24::setPipe(uint8 pipe, bool enable){
+	//Pipe starts from 0 to 5
+	uint8 val = readRegister(NRF24_REG_EN_RXADDR);	
+	if (pipe < 6){
+		if (enable)
+			val |= (1 << pipe);
+		else
+			val &= ~(1 << pipe);		
+		writeRegister(NRF24_REG_EN_RXADDR, val);
+	}
+}
+
+void Nrf24::enableDynamicPayload(bool enable){
+	if (enable){
+		//DPL_P0 must be enabled
+		writeRegister(NRF24_REG_DYNPD, 0xFF);
+
+		//Set EN_DPL bit
+		writeRegister(NRF24_REG_FEATURE, (1<<2));
+	}	
+	_dynamicPayload = enable;
+}
+
+
+void Nrf24::flushTx(void){
+	execCmd(NRF24_CMD_FLUSH_TX, NRF24_WRITE, 0, 0);
+}
+
+void Nrf24::flushRx(void){
+	execCmd(NRF24_CMD_FLUSH_RX, NRF24_WRITE, 0, 0);
+}
+
+void Nrf24::readRegister(byte reg_address, byte* buffer, uint8 length){
+     //Execute R_REGISTER command; Returns maximum 5 bytes
+	 length = (length > 5) ? 5 : length;
+     execCmd(NRF24_CMD_R_REGISTER + reg_address, NRF24_READ, buffer, length);
+}
+byte Nrf24::readRegister(byte reg_address){
+	//Single register read
+	byte buffer;
+	execCmd(NRF24_CMD_R_REGISTER + reg_address, NRF24_READ, &buffer, 1);
+	return buffer;
+	
+}
+void Nrf24::writeRegister(byte reg_address, byte data){
+	//Single register write
+    execCmd(NRF24_CMD_W_REGISTER + reg_address, NRF24_WRITE, &data, 1);
+}
+
+void Nrf24::writeRegister(byte reg_address, byte* data, uint8 length){
+     //Execute W_REGISTER command; Returns maximum 5 bytes
+	 length = (length > 5) ? 5 : length;
+     execCmd(NRF24_CMD_W_REGISTER + reg_address, NRF24_WRITE, data, length);
+}
+
+
+void Nrf24::execCmd(byte cmd, Nrf24RW read_write, byte* buffer, uint8 length){
+	digitalWrite(_port.csn, LOW);	 
+	
+	//Used "transfer" to skip the first byte (STATUS)
+	_port.spi->transfer(cmd);	
+	
+	if (buffer){
+		if (read_write == NRF24_READ){
+			while(length--)
+				*buffer++ = _port.spi->transfer(0x00);	//Sending dummy byte to generate clock
+		} else if (read_write == NRF24_WRITE){
+			while(length--)
+				_port.spi->write(*buffer++);
+		}
+	}
+	digitalWrite(_port.csn, HIGH);	 
+}
+
+/*
+void Nrf24::read(uint8 cmd, uint8* buffer, uint8 length){
+	digitalWrite(_port.csn, LOW);	 
+	_port.spi->write(cmd);
+	if (buffer){
+		while(length--)
+			*buffer++ = _port.spi->read();
+	}
+	digitalWrite(_port.csn, HIGH);	 
+}
+
+void Nrf24::write(uint8 cmd, uint8* data, uint8 length){
+	digitalWrite(_port.csn, LOW);	 
+	_port.spi->write(cmd);
+	if (data){
+		while(length--)
+			_port.spi->write(*data++);
+	}
+	digitalWrite(_port.csn, HIGH);	 
+}
+*/
+
+void Nrf24::_setBit(byte* reg, uint8 bit){
+	*reg |= (1 << bit);
+}
+void Nrf24::_unsetBit(byte* reg, uint8 bit){
+	*reg &= ~(1 << bit);
+}
+
+void Nrf24::setMode(Nrf24Mode mode){
+     uint8 config, powerStatus;
+	 
+	 config = readRegister(NRF24_REG_CONFIG);	 
+	 powerStatus = config & NRF24_CONFIG_PWR_UP;
+     
+     if (mode == NRF24_MODE_OFF){
+        config &= ~(1 << 1);
+     } else {
+        config |= NRF24_CONFIG_PWR_UP;
+        switch (mode){
+           case NRF24_MODE_RX :
+                config |= NRF24_CONFIG_PRIM_RX;
+				digitalWrite(_port.ce, HIGH);
+                break;
+
+           case NRF24_MODE_STANDBY2 :
+                flushRx();
+                digitalWrite(_port.ce, HIGH);
+                break;
+		   
+		   default:
+           case NRF24_MODE_STANDBY1 :
+                flushTx();
+                
+           case NRF24_MODE_TX :
+                config &= ~NRF24_CONFIG_PRIM_RX;
+                digitalWrite(_port.ce, LOW);
+                break;				
+        }
+     }
+     writeRegister(NRF24_REG_CONFIG, config);
+     
+    //  According to datasheet the device needs at least 
+	//	1.5mS to go to standby-I from power down mode otherwise 130uS	 
+     if(powerStatus)
+         delayMicroseconds(150);
+     else
+         delay(2);
+}
---
+++ Nrf24.h
@@ -0,0 +1,190 @@
+/******************************************************************************
+ * The MIT License
+ *
+ * Copyright (c) 2014 Nasir Ahmad.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *****************************************************************************/
+
+/**
+ *  @brief NRF24L01+ RF module library for LeafLabs Maple
+ */
+ 
+ 
+#ifndef _NRF24_H_
+#define _NRF24_H_
+
+
+#include "libmaple_types.h"
+#include "HardwareSPI.h"
+#include "io.h"
+#include "delay.h"
+#include "wirish.h"
+
+
+/* NRF24 Commands */
+#define NRF24_CMD_R_REGISTER         0x00
+#define NRF24_CMD_W_REGISTER         0x20
+#define NRF24_CMD_R_RX_PAYLOAD       0x61
+#define NRF24_CMD_W_TX_PAYLOAD       0xA0
+#define NRF24_CMD_FLUSH_TX           0xE1
+#define NRF24_CMD_FLUSH_RX           0xE2
+#define NRF24_CMD_REUSE_TX_PL        0xE3
+#define NRF24_CMD_R_RX_PL_WID        0x60
+#define NRF24_CMD_W_ACK_PAYLOAD      0xA8
+#define NRF24_CMD_W_TX_PAYLOAD_NOACK 0xB0
+#define NRF24_CMD_NOP                0xFF
+#define NRF24_DUMMY_BYTE         	 0x00
+
+
+/* NRF24 Register addresses */
+#define  NRF24_REG_CONFIG       0x00
+#define  NRF24_REG_EN_AA        0x01
+#define  NRF24_REG_EN_RXADDR    0x02
+#define  NRF24_REG_SETUP_AW     0x03
+#define  NRF24_REG_SETUP_RETR   0x04
+#define  NRF24_REG_RF_CH        0x05
+#define  NRF24_REG_RF_SETUP     0x06
+#define  NRF24_REG_STATUS       0x07
+#define  NRF24_REG_OBSERVE_TX   0x08
+#define  NRF24_REG_CD           0x09
+#define  NRF24_REG_RX_ADDR_P0   0x0A
+#define  NRF24_REG_RX_ADDR_P1   0x0B
+#define  NRF24_REG_RX_ADDR_P2   0x0C
+#define  NRF24_REG_RX_ADDR_P3   0x0D
+#define  NRF24_REG_RX_ADDR_P4   0x0E
+#define  NRF24_REG_RX_ADDR_P5   0x0F
+#define  NRF24_REG_TX_ADDR      0x10
+#define  NRF24_REG_RX_PW_P0     0x11
+#define  NRF24_REG_RX_PW_P1     0x12
+#define  NRF24_REG_RX_PW_P2     0x13
+#define  NRF24_REG_RX_PW_P3     0x14
+#define  NRF24_REG_RX_PW_P4     0x15
+#define  NRF24_REG_RX_PW_P5     0x16
+#define  NRF24_REG_FIFO_STATUS  0x17
+#define  NRF24_REG_DYNPD        0x1C
+#define  NRF24_REG_FEATURE      0x1D
+
+
+
+/* NRF24 Config register bits */
+#define  NRF24_CONFIG_PRIM_RX		0x01
+#define  NRF24_CONFIG_PWR_UP		0x02
+#define  NRF24_CONFIG_CRCO			0x04
+#define  NRF24_CONFIG_EN_CRC		0x08
+#define  NRF24_CONFIG_MASK_MAX_RT	0x10
+#define  NRF24_CONFIG_MASK_TX_DS	0x20
+#define  NRF24_CONFIG_MASK_RX_DR	0x40
+
+
+/* Nrf24 Status register bits */
+#define  NRF24_STATUS_TX_FULL   0x00
+#define  NRF24_STATUS_RX_P_NO   0x0E
+#define  NRF24_STATUS_MAX_RT    0x10
+#define  NRF24_STATUS_TX_DS     0x20
+#define  NRF24_STATUS_RX_DR     0x40
+
+typedef enum {
+	PRIM_RX		= 0,
+	PWR_UP		= 1,
+	CRCO		= 2,
+	EN_CRC	 	= 3,
+	MASK_MAX_RT = 4,
+	MASK_TX_DS	= 5,
+	MASK_RX_DR	= 6
+} NRF24_CONFIG;
+
+typedef enum {
+	NRF24_MODE_TX, 
+	NRF24_MODE_RX, 
+	NRF24_MODE_STANDBY1, 
+	NRF24_MODE_STANDBY2, 
+	NRF24_MODE_OFF
+} Nrf24Mode;
+
+typedef enum {
+	NRF24_DR_256KBPS, 
+	NRF24_DR_1MBPS, 
+	NRF24_DR_2MBPS
+} Nrf24DataRate;
+	
+typedef enum{
+	NRF24_WRITE = 0,
+	NRF24_READ  = 1
+} Nrf24RW;
+
+class Nrf24 {	
+	public:
+		typedef	struct {
+			HardwareSPI* 	spi;
+			uint8			csn;
+			uint8			ce;
+			uint8			irq;
+		} Port;	
+		
+		Nrf24(HardwareSPI*, uint8, uint8);
+		void begin(Nrf24Mode);
+		
+		/* SPI functions */
+		void readRegister(byte, byte*, uint8);
+		byte readRegister(byte);
+		void writeRegister(byte, byte*, uint8);
+		void writeRegister(byte, uint8);
+		
+		/* Configure device */
+		void setAutoRetransmit(uint8);
+		void setDateRate(Nrf24DataRate);
+		void setPayloadSize(uint8, uint8);
+		void setPipe(uint8, bool);
+		void enableDynamicPayload(bool);
+		
+		/* Data send/receive */
+		void send(byte*, uint8);	
+		void read(byte*, uint8);
+		uint8 txDataSent();
+		uint8 available();
+		
+		void execCmd(byte, Nrf24RW, byte*, uint8);
+		void flushTx(void);
+		void flushRx(void);
+		void setMode(Nrf24Mode);
+		
+		/* Interrupt */
+		void onInterrupt();		
+		uint8 getIRQPin();
+		void useIRQPin(uint8, bool);		
+		void attachOnPayloadReceiveCallback(void (*)(void));
+		
+	private:
+		Port _port;
+		volatile bool _dynamicPayload;
+		volatile bool _useInterrupt;
+		
+		volatile byte _status;
+		
+		void (*_onPayloadReceiveCallback)(void);
+		void _fetchStatus();
+		void _setBit(byte*, uint8);
+		void _unsetBit(byte*, uint8);		
+};
+
+
+#endif
